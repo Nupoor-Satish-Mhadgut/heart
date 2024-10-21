840a8473d87a92bc0a8ebaf18eca6d1d46c1eb09
Nupoor-Satish-Mhadgut <147414164+Nupoor-Satish-Mhadgut@users.noreply.github.com>
2024-10-21 11:47:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,31 +1,4 @@
-from flask import Flask, request, jsonify, render_template
-import pickle
-import numpy as np
 
-app = Flask(__name__)
-
-# Load your trained logistic regression model
-with open('D:/Users/user/Desktop/Heart Disease Prediction/model2.pkl', 'rb') as file:
-    model = pickle.load(file)
-# model = pickle.load(open('D:/Users/user/Desktop/Heart Disease Prediction/model2.pkl', 'rb'))
-
-# Home route to serve the HTML form
-@app.route('/')
-def home():
-    return render_template('heart disease1.html')  # Connect to your form page here
-
-# Prediction route for handling form data
-@app.route('/predict', methods=['POST'])
-def predict():
-    # Get data from the form
-    data = request.form
-
-    # Convert form values to appropriate types (ensure these match the order expected by the model)
-    input_features = [int(data['age']),
-                      int(data['sex']),
-                      int(data['chest_pain']),
-                      int(data['bp']),
-                      int(data['cholesterol']),
                       int(data['fbs']),
                       int(data['ekg']),
                       int(data['max_hr']),
